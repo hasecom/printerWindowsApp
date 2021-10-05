b3cb76f97f47f7fc90b0fbf6d732e24f3089d85f
hasecom <hasetennisth@gmail.com>
2021-10-05 22:30:27 +0900

    プロジェクト ファイルを追加します。

---
+++ printerWindowsApp/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace printerWindowsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.getPrinterInfo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(213, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(288, 184);
+            this.listBox1.TabIndex = 0;
+            // 
+            // getPrinterInfo
+            // 
+            this.getPrinterInfo.Location = new System.Drawing.Point(558, 159);
+            this.getPrinterInfo.Name = "getPrinterInfo";
+            this.getPrinterInfo.Size = new System.Drawing.Size(197, 37);
+            this.getPrinterInfo.TabIndex = 1;
+            this.getPrinterInfo.Text = "現在のプリンターを取得";
+            this.getPrinterInfo.UseVisualStyleBackColor = true;
+            this.getPrinterInfo.Click += new System.EventHandler(this.getPrinterInfo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.getPrinterInfo);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button getPrinterInfo;
+    }
+}
+
---
+++ printerWindowsApp/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace printerWindowsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void getPrinterInfo_Click(object sender, EventArgs e)
+        {
+
+            Process p = new System.Diagnostics.Process();
+            p.StartInfo.FileName = @"C:\dev\printerWebApp\getPrinterInfo.bat";
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardInput = false;
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.Arguments = @"/c dir c:\ /w";
+            p.Start();
+            string results = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+            p.Close();
+
+            MatchCollection matche = Regex.Matches(results, @"プリンター名\s(.*)|ドライバー名\s(.*)|ポート名\s(.*)");
+
+            foreach (Match m in matche)
+            {
+          
+                Console.WriteLine(m.Value);
+            }
+
+        }
+    }
+}
